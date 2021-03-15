4b5743c5f7be22e5bd2da91ea11ca48301adfd98
RamBasDev <Bascontrol20@users.noreply.github.com>
2021-03-15 07:36:13 +0700

    Add project files.

---
+++ FX3U Visual/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FX3U_Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txt_addD = new System.Windows.Forms.TextBox();
+            this.txt_ValD = new System.Windows.Forms.TextBox();
+            this.txt_addM = new System.Windows.Forms.TextBox();
+            this.txt_valM = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(287, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Write data(D)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(287, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Read data(D)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(287, 207);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Wire data (M)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(287, 236);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Read data (M)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // txt_addD
+            // 
+            this.txt_addD.Location = new System.Drawing.Point(452, 92);
+            this.txt_addD.Name = "txt_addD";
+            this.txt_addD.Size = new System.Drawing.Size(100, 20);
+            this.txt_addD.TabIndex = 6;
+            // 
+            // txt_ValD
+            // 
+            this.txt_ValD.Location = new System.Drawing.Point(452, 122);
+            this.txt_ValD.Name = "txt_ValD";
+            this.txt_ValD.Size = new System.Drawing.Size(100, 20);
+            this.txt_ValD.TabIndex = 7;
+            // 
+            // txt_addM
+            // 
+            this.txt_addM.Location = new System.Drawing.Point(452, 210);
+            this.txt_addM.Name = "txt_addM";
+            this.txt_addM.Size = new System.Drawing.Size(100, 20);
+            this.txt_addM.TabIndex = 8;
+            // 
+            // txt_valM
+            // 
+            this.txt_valM.Location = new System.Drawing.Point(452, 239);
+            this.txt_valM.Name = "txt_valM";
+            this.txt_valM.Size = new System.Drawing.Size(100, 20);
+            this.txt_valM.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_valM);
+            this.Controls.Add(this.txt_addM);
+            this.Controls.Add(this.txt_ValD);
+            this.Controls.Add(this.txt_addD);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txt_addD;
+        private System.Windows.Forms.TextBox txt_ValD;
+        private System.Windows.Forms.TextBox txt_addM;
+        private System.Windows.Forms.TextBox txt_valM;
+    }
+}
+
---
+++ FX3U Visual/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ActUtlTypeLib;
+namespace FX3U_Visual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        connection bien1 = new connection();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bien1.connect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bien1.disconnect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bien1.plc.WriteDeviceBlock(txt_addD.Text, 1, Convert.ToInt16(txt_ValD.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int biennhan;
+            bien1.plc.ReadDeviceBlock(txt_addD.Text, 1,out biennhan );
+            txt_ValD.Text = biennhan.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bien1.plc.SetDevice(txt_addM.Text, Convert.ToInt16(txt_valM.Text));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int biennhanM;
+            bien1.plc.GetDevice(txt_addM.Text, out biennhanM);
+            txt_valM.Text=biennhanM.ToString();
+        }
+    }
+    public class connection
+    {
+        public ActUtlType plc = new ActUtlType();
+        public int iret;
+        public void connect()
+        {
+            plc.ActLogicalStationNumber = 300; // Station number
+            plc.Open();
+        }
+        public void disconnect()
+        {
+            plc.Close();
+        }
+
+    }
+}
